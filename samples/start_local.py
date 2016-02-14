aaad2688a1ce745b56f2be27928fe2a816dd6c89
zuev <zuev@scrapinghub.com>
2016-02-14 16:29:06 +0300

    script sample update

--- samples/start_local.py
+++ samples/start_local.py
@@ -8,11 +8,10 @@ if __name__ == '__main__':
     url = 'http://127.0.0.1:8888/crawler/start'
     logging.basicConfig(level=logging.INFO)
     headers = {"Content-Type": "application/json"}
-    # data = {"domain":"spider://tortest","options": {"args":{}}}
-    data = {"domain":"spider://localtest","options":{"args":{"username":"sammy",
+    data = {"domain":"spider://ar_phpbb","options":{"args":{"username":"sammy",
                                                              "password":"badpassword",
                                                              "start_urls":["http://127.0.0.1"],
-                                                             "test_key":"22"},
+                                                             "post_days":-1},
                                                      "settings":{}}}
     r = requests.post(url, data=json.dumps(data), headers=headers)
     print r
